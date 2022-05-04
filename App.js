bfcb13d4b0868b2481ccc5a458d18ab846ca07c8
Danielle Souza <dany.sa1@hotmail.com>
2022-05-04 13:26:01 -0300

    feat: incluindo imagens

--- App.js
+++ App.js
@@ -1,22 +1,15 @@
-import { StatusBar } from "expo-status-bar";
+
 import react from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar, StyleSheet,  SafeAreaView } from "react-native";
 import Cesta from "./src/telas/Cesta";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
+    <SafeAreaView>
+      <StatusBar />
       <Cesta />
-    </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+
